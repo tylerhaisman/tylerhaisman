f882916837c9b5189fc38dfdd6629e2cee0e42c9
Tyler Haisman <tyler@haisman.net>
2024-05-14 01:31:04 -0500

    Adding documentation page for pneumonia prediction project and framework to convert .md files to tsx

--- app/page.tsx
+++ app/page.tsx
@@ -109,13 +109,16 @@ export default function Home() {
                     problem-solving.
                   </motion.p>
                   <motion.button
-                    className="mt-8 flex items-start duration-200"
+                    className="mt-8 flex items-start"
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: 0.4 }}
                   >
-                    <Link href={"#projects"}>
-                      <div className="flex flex-col justify-center items-center border border-white rounded-full w-20 h-20 cursor-pointer">
+                    <Link
+                      href={"#projects"}
+                      className="hover:translate-y-[-4px] duration-200"
+                    >
+                      <div className="flex flex-col justify-center items-center border border-white rounded-full w-20 h-20">
                         <div className="h-10 w-1 bg-white border-none mt-2"></div>
                         <Image
                           src={Arrow}
@@ -185,14 +188,19 @@ export default function Home() {
                     </div>
                   </div>
                   <div className="flex gap-2 flex-wrap">
-                    {/* <button className="border border-gray-50 rounded-full px-4 py-2 duration-200 flex gap-2 justify-center items-center">
+                    <button
+                      className="border border-gray-50 rounded-full px-4 py-2 duration-200 flex gap-2 justify-center items-center"
+                      onClick={() =>
+                        window.open("/pneumonia_prediction_utility", "_blank")
+                      }
+                    >
                       <Image
                         src={Documentation}
                         alt="Documentation"
                         className="w-4 h-4"
                       ></Image>
                       <p className="text-lg">READ MORE</p>
-                    </button> */}
+                    </button>
                     <button
                       className="border border-gray-50 rounded-full px-4 py-2 duration-200 flex gap-2 justify-center items-center relative"
                       onClick={() =>
---
+++ public/markdown/pneumonia_prediction_utility.ts
@@ -0,0 +1,57 @@
+const markdown = `
+# Overview
+My pneumonia prediction utility utilizes deep learning models to analyze chest X-ray images and predict whether pneumonia is present and, if so, whether it is bacterial or viral in nature.
+
+# Technology Stack
+## Frontend:
+  - Framework: Next.js (React)
+  - Styling: Tailwind CSS
+  - Language: TypeScript
+## Backend:
+  - Framework: Django (Python)
+  - Deep Learning Library: TensorFlow Keras
+
+# Models
+## 1. Chest X-ray Detection Model:
+   - This model determines whether the image is a chest X-ray or not.
+   - Trained on nearly 2000 chest X-ray images.
+   - Utilizes a convolutional neural network (CNN) architecture.
+   
+## 2. Pneumonia Classification Model:
+   - This model identifies if pneumonia is present and categorizes it as bacterial or viral.
+   - Trained on over 4000 chest X-ray images depicting normal, bacterial pneumonia, and viral pneumonia cases.
+   - Utilizes a CNN architecture.
+
+# Data Sources
+## Chest X-Ray Images (Pneumonia) Dataset:
+  - Kaggle Dataset: [Chest X-Ray Images (Pneumonia)](https://www.kaggle.com/paultimothymooney/chest-xray-pneumonia)
+  - Contains chest X-ray images depicting normal, bacterial pneumonia, and viral pneumonia cases.
+  - Kermany, Daniel; Zhang, Kang; Goldbaum, Michael (2018), “Labeled Optical Coherence Tomography (OCT) and Chest X-Ray Images for Classification”, Mendeley Data, V2, doi: 10.17632/rscbjbr9sj.2
+
+## Random Images Dataset:
+  - Kaggle Dataset: [Unsplash Random Images Collection](https://www.kaggle.com/lprdosmil/unsplash-random-images-collection)
+  - Used for negative samples (i.e., images that are not chest X-rays).
+
+# Workflow
+## 1. Image Verification:
+   - The API first verifies if the uploaded image is a chest X-ray using the Chest X-ray Detection Model.
+   
+## 2. Pneumonia Prediction:
+   - If the image is confirmed to be a chest X-ray, it is sent to the Pneumonia Classification Model.
+   - The model predicts whether pneumonia is present and its type (bacterial or viral).
+   
+## 3. Result Display:
+   - The prediction results are sent back to the frontend for display.
+   - Users can then interpret the prediction and act accordingly.
+
+# Future Directions
+## Enhanced Model Accuracy:
+  Continuous training and fine-tuning of models to improve accuracy and robustness.
+  ## Expansion of Dataset:
+  Incorporating more diverse and extensive datasets to capture a wider range of cases and improve generalization.
+
+# About the Author
+This project was developed by Tyler Haisman, a computer science student aspiring to work in bioinformatics. For inquiries or collaborations, please contact me by clicking on the "CONTACT" button in the header.
+`;
+
+export default markdown;
