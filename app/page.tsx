d4bd513f5eb9864a9750dd05765895e14c7db2a9
Tyler Haisman <tyler@haisman.net>
2025-02-12 00:58:36 -0500

    Updating resume

--- app/page.tsx
+++ app/page.tsx
@@ -994,7 +994,7 @@ export default function Home() {
                       className="border border-black rounded-full px-4 py-2 duration-200 flex items-center justify-center gap-2 mt-2"
                       onClick={() =>
                         window.open(
-                          "documents/TylerHaismanResume_2.pdf",
+                          "documents/TylerHaismanResume3.pdf",
                           "_blank"
                         )
                       }
